03571d6db0a049e25477b2a074d83aac6d629e02
avijit sarkar <insetavijit@gmail.com>
2018-09-01 23:49:57 +0530

    drawers are intigrated + added drawer

--- ASSETS/jScripts/modules/mdc_comp_init.js
+++ ASSETS/jScripts/modules/mdc_comp_init.js
@@ -3,6 +3,7 @@
 import {MDCRipple} from '@material/ripple';
 import {MDCTemporaryDrawer} from '@material/drawer';
 
+    var drawer = new MDCTemporaryDrawer($(".mdc-drawer")[0])
 
 jQuery(document).ready(($)=>{
     ///adding ripple to eatch button 
@@ -12,7 +13,7 @@ jQuery(document).ready(($)=>{
 
     
     // left - side bar ( + nav - drawer )
-    let drawer = new MDCTemporaryDrawer($(".mdc-drawer")[0])
+    
     $('.drawer-mdc').on('click', ()=> {
         drawer.open = true;
     });
